245d654848297fdcd7cf0fb42beb3d35421937e4
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-12-16 23:14:24 +0500

    Portfolio using tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,22 @@ import Image from "next/image";
 const Home = () => {
   return (
     <Layout>
-      <section className="text-center px-2 py-2 space-y-8">
-        <div className="flex justify-center items-center p-6">
-          <h1 className="text-5xl font-extrabold text-gradient p-2 text-center px-6 py-1 space-y-8 text-primary">
+      <section className="text-center px-4 py-8 space-y-8">
+        
+        <div className="flex flex-col md:flex-row justify-center items-center md:space-x-8 p-6">
+          <h1 className="text-3xl sm:text-5xl font-extrabold text-gradient p-5 m-5 text-center md:text-left text-primary">
             I am Abdullah Arif, Welcome to My Portfolio.
           </h1>
-          <Image
-            src={"/me.jpg"}
-            height={300}
-            width={300}
-            alt={"me"}
-            className="p-2 rounded-full shadow-lg"
-          ></Image>
+          <Image 
+            src={"/me.jpg"} 
+            height={300} 
+            width={300} 
+            alt={"me"} 
+            className="p-2 rounded-full shadow-lg max-w-full h-auto"
+          />
         </div>
-        <p className="text-xl font-medium text-gray-700 max-w-2xl mx-auto text-center px-6 py-12 space-y-8">
+
+        <p className="text-lg sm:text-xl font-medium text-gray-700 max-w-2xl mx-auto text-center px-6 py-8">
           I am currently enrolled in a comprehensive course at Governor House
           focused on Web3, TypeScript, and Next.js, which involves months of
           intensive coding practice. This course covers advanced concepts in
@@ -27,12 +29,16 @@ const Home = () => {
           to build a strong foundation in modern web development with a focus on
           blockchain and decentralized technologies.
         </p>
-        <a
-          href="/projects"
-          className="bg-primary text-center space-y-8 hover:bg-secondary text-white font-semibold py-3 px-8 rounded-md shadow-lg transition duration-300 ease-in-out"
-        >
-          View My Projects
-        </a>
+
+        <div className="flex justify-center">
+          <a 
+            href="/projects" 
+            className="bg-primary hover:bg-secondary text-white font-semibold py-3 px-8 rounded-md shadow-lg transition duration-300 ease-in-out text-center"
+          >
+            View My Projects
+          </a>
+        </div>
+
       </section>
     </Layout>
   );
